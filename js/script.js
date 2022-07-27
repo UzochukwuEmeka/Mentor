00a9935e99fb0887859d9534bded20049f449870
Ife Jeremiah <jerryroboy16@gmail.com>
2022-07-27 20:47:24 +0100

    Fixed responsiveness for overview dashboard

--- js/script.js
+++ js/script.js
@@ -1,7 +1,27 @@
 const selectors = document.querySelectorAll('.con-selector .secondary-button')
 
+const sidebar = document.querySelector('.con-nav-sidebar')
+const menu = document.getElementById('first-menu')
+
 selectors.forEach(selector => addEvent(selector))
 
 function addEvent(list) {
   list.addEventListener('click', () => { list.classList.toggle('active') })
+}
+
+menu.addEventListener('click', slideIn)
+
+document.addEventListener('click', slideClose)
+
+function slideIn() {
+  const run = setTimeout(slideOpen, 1)
+}
+
+function slideOpen() {
+  sidebar.classList.add('active')
+}
+
+function slideClose(evt) {
+  const target = evt.target.closest('.nav-sidebar')
+  if (!target) sidebar.classList.remove('active')
 }
